97fbc0a51cae0a29b416e29d4f235077f1fa6eeb
Mikedicpro <77217801+Mikedicpro@users.noreply.github.com>
2021-09-02 21:02:20 -0500

    w1d4homework

--- d14arrays/arrays.js
+++ d14arrays/arrays.js
@@ -1,8 +1,8 @@
   
 "use strict";
 
-/* You need the module.exports when testing in node.  Comment it out when you send your file to the browser
-*/ 
+// You need the module.exports when testing in node.  Comment it out when you send your file to the browser
+
 module.exports = {maxOfThree, sum, multiply }; //add all of your function names here that you need for the node mocha tests
 
 /**
@@ -13,7 +13,16 @@ module.exports = {maxOfThree, sum, multiply }; //add all of your function names 
  * @returns {number} largest of a, b, c
  */
 function maxOfThree(a, b, c){ 
-    return c;
+   let max_value=0;
+    if(a>b){
+        max_value=a;
+    }else{
+        max_value=b;
+    }
+    if (c>max_value){
+       max_value=c;
+    }
+    return max_value;
 }
 
 /**
@@ -27,6 +36,7 @@ function sum(arr){
         tot += number;
     }
     return tot;
+    
 }
 
 
@@ -36,8 +46,48 @@ function sum(arr){
  * @returns {number} sum of arr numbers
  */
 function multiply(arr){
-    let tot = 1;
-
+    let tot =1;
+    for (const number of arr){
+        tot*=number;
+    }
     return tot;
 
 }
+/**
+ * 
+ * @param {arr} array of nummbers 
+ * @returns reversed charactor.
+ */
+function reverseArrayInPlace(arr){
+    let temp;
+    let count = 0;
+    for(let i=arr.length-1; i>=0; i--){
+        temp = arr[i];
+        arr[i] = arr[count];
+        arr[count] = temp;
+        if (count === i && arr.length%2 !== 0 || count + 1 === i && arr.length%2 === 0){
+            break;
+        }
+        count++;
+    }
+    return arr;
+
+console.log(reverseArrayInPlace([1,2,3,4,5]));
+}
+console.log(reverseArrayInPlace([1,2,3,4,5]));
+
+/**
+ * 
+ * @param {arr } arrary of numbers 
+ * @returns {max} maxword longest word
+ */
+function findLongestWord(arr) {
+    let maxWord = 0;
+    for (let word of arr) {
+      if (word.length > maxWord) {
+        maxWord = word.length;
+      }
+    }
+    return maxWord;
+  }
+  console.log(findLongestWord(["chair", "car", "computer", "school"]));
