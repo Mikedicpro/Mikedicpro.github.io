0f9c43d75b3d4fcef0b34c9397a8214413a35e2a
Mikedicpro <77217801+Mikedicpro@users.noreply.github.com>
2021-09-06 16:33:27 -0500

    finishing myMap

--- d15fnExps/fnExps.js
+++ d15fnExps/fnExps.js
@@ -2,7 +2,7 @@
 
 /* You need the module.exports when testing in node.  Comment it out when you send your file to the browser
 */ 
-// module.exports = {double, times100, myMap }; //add all of your function names here that you need for the node mocha tests
+ //module.exports = {double, times100, myMap }; //add all of your function names here that you need for the node mocha tests
 
 /**
  *@returns {number} double the input
@@ -27,22 +27,35 @@
 
  let arr=[2,3,4,5];
 
- function doubleArr(arr){
-     for (let i =0;i<arr.length;i++){
-         arr[i]*=2;// arr[i]=arr[i]*2
-     }
-     return arr;
-   // console.log("Expected result ..2,3,4,5 : will be 4,6,8,10" +doubleArr(arr));
- }
-console.log("Expected result ..2,3,4,5 : will be 4,6,8,10 = " + doubleArr(arr));
-
-function myMap(arr1,double,hunredTimes){
-    for(let i=0;i<arr.length;i++){
-        arr[i]*=100;
+function myMap(arr,fun){
+    let newArray=[];
+    for (let i=0;i<arr.length;i++){
+        newArray.push(fun([i]));
     }
-    return arr1;
-
+    return newArray;
 }
 
-console.log("Expected result multiply by 2& then *100..400,600,800,1000 will be =   "+myMap(arr));
+
+
+
+//  let arr=[2,3,4,5];
+
+//  function doubleArr(arr){
+//      for (let i =0;i<arr.length;i++){
+//          arr[i]*=2;// arr[i]=arr[i]*2
+//      }
+//      return arr;
+//    // console.log("Expected result ..2,3,4,5 : will be 4,6,8,10" +doubleArr(arr));
+//  }
+// console.log("Expected result ..2,3,4,5 : will be 4,6,8,10 = " + doubleArr(arr));
+
+// function myMap(arr1,double,hunredTimes){
+//     for(let i=0;i<arr.length;i++){
+//         arr[i]*=100;
+//     }
+//     return arr1;
+
+// }
+
+// console.log("Expected result multiply by 2& then *100..400,600,800,1000 will be =   "+myMap(arr));
 
